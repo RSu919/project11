327629c0d55facd4f7b05ccbefc41370bb21c90f
Rsu919 <rsu200@gmail.com>
2026-02-05 20:20:01 +0800

    除錯02

--- survey.js
+++ survey.js
@@ -5,4 +5,5 @@ const supabase = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1iZGF0YndycmFsaGxraHloeGxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxNjg2OTksImV4cCI6MjA4NTc0NDY5OX0.5kv8UvBRbYfcZGLXdKI_cWtplkN3YT05XC5AUhVtsok"
 );
 
+
 console.log("supabase init ok");
